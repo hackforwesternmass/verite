e8a8d43a858da64d638e38ffb77b212dbe5c5578
Andrew Pasquale <andrew@elytra.net>
2018-01-10 11:03:24 -0600

    Add menu items

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,9 +38,11 @@
 
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
+                    <!--
                     <ul class="nav navbar-nav">
                         &nbsp;
                     </ul>
+                    -->
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
@@ -49,6 +51,8 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
+                            <li><a href="{{ route('list.audits') }}">Audits</a></li>
+                            <li><a href="{{ route('list.companies') }}">Companies</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
